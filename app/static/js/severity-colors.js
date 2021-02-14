9b1b42005e3a0b5496a18159e3dec564ec599fab
lafiamafia <lafiamafia@protonmail.com>
2021-02-14 15:07:10 -0500

    It works!

--- app/static/js/severity-colors.js
+++ app/static/js/severity-colors.js
@@ -1,13 +1,37 @@
 // Run function on load
 window.addEventListener("load", function(){
   // Info
-  document.getElementsByName("Info").style.backgroundColor = '#0071B9';
+  var info = document.getElementsByName("Info")
+  var i;
+  for (i = 0; i < info.length; i++) {
+    info[i].classList.add('info');
+  }
+  
   // Low
-  document.getElementsByName("Low").style.backgroundColor = '#3FAE49';
+  var low = document.getElementsByName("Low")
+  var l;
+  for (l = 0; l < low.length; l++) {
+    low[l].classList.add('low');
+  }
+
   // Medium
-  document.getElementsByName("Medium").style.backgroundColor = '#FDC431';
+  var medium = document.getElementsByName("Medium")
+  var m;
+  for (m = 0; m < medium.length; m++) {
+    medium[m].classList.add('medium');
+  }
+
   // High
-  document.getElementsByName("High").style.backgroundColor = '#EE9336';
+  var high = document.getElementsByName("High")
+  var h;
+  for (h = 0; h < high.length; h++) {
+    high[h].classList.add('high');
+  }
+
   // Critical
-  document.getElementsByName("Critical").style.backgroundColor = '#D43F3A';
+  var critical = document.getElementsByName("Critical")
+  var c;
+  for (c = 0; c < critical.length; c++) {
+    critical[c].classList.add('critical');
+  }
 });
